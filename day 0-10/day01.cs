787eeb4a939317b3e3c7a88c3f5370b0ff13a62b
Istiaq-hossain-shawon <hossain.ict@gmail.com>
2019-03-27 10:52:19 +0600

    solve day 01 problem

---
+++ day 0-10/day01.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Solution {
+    static void Main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        
+        // Declare second integer, double, and String variables.
+        int a;
+        double b;
+        string c;
+        // Read and save an integer, double, and String to your variables.
+        a = Convert.ToInt32(Console.ReadLine());        
+        b =  Convert.ToDouble(Console.ReadLine());
+        c = Console.ReadLine();
+        // Print the sum of both integer variables on a new line.
+        Console.WriteLine(a+i);
+        // Print the sum of the double variables on a new line.
+        Console.WriteLine(((float)b+(float)d).ToString("0.0"));
+        
+        // Concatenate and print the String variables on a new line
+        // The 's' variable above should be printed first.
+        Console.WriteLine(String.Concat(s,c));
+
+
+    }
